15138b1b9cef1b0609ea8e1885d58a7137d405a7
Lyzi Diamond <lyzidiamond@gmail.com>
2014-03-05 17:36:03 -0800

    making chaaaanges

--- neighborhoods/neighborhoods.js
+++ neighborhoods/neighborhoods.js
@@ -1,5 +1,5 @@
 // initialize map
-var map = L.mapbox.map('neighborhoods-map', 'codeforamerica.hek4o94g', {maxZoom: 8, minZoom: 4}).setView([38.042,-84.515], 8);
+var map = L.mapbox.map('neighborhoods-map', 'codeforamerica.hek4o94g', {maxZoom: 12, minZoom: 10}).setView([38.042,-84.515], 10);
 
 function onEachFeature(feature, layer) {
   var popupContent = "<strong>" + feature.properties.Assoc_Name + "</strong>" + "<br>Council District: District " + feature.properties.DISTRICT + "<br>Council Member: " + feature.properties.REP + "<br>Phone Number: " + feature.properties.TELEPHONE + "<br> Email: " + feature.properties.EMAIL;
@@ -11,18 +11,18 @@ function onEachFeature(feature, layer) {
 
 function style(feature) {
   switch (feature.properties.DISTRICT) {
-    case '1': return {color: "#8dd3c7"};
-    case '2': return {color: "#ffffb3"};
-    case '3': return {color: "#bebada"};
-    case '4': return {color: "#fb8072"};
-    case '5': return {color: "#80b1d3"};
-    case '6': return {color: "#fdb462"};
-    case '7': return {color: "#b3de69"};
-    case '8': return {color: "#fccde5"};
-    case '9': return {color: "#d9d9d9"};
-    case '10': return {color: "#bc80bd"};
-    case '11': return {color: "#ccebc5"};
-    case '12': return {color: "#ffed6f"};
+    case 1: return {color: "#8dd3c7"};
+    case 2: return {color: "#ffffb3"};
+    case 3: return {color: "#bebada"};
+    case 4: return {color: "#fb8072"};
+    case 5: return {color: "#80b1d3"};
+    case 6: return {color: "#fdb462"};
+    case 7: return {color: "#b3de69"};
+    case 8: return {color: "#fccde5"};
+    case 9: return {color: "#d9d9d9"};
+    case 10: return {color: "#bc80bd"};
+    case 11: return {color: "#ccebc5"};
+    case 12: return {color: "#ffed6f"};
   };
 };
 
