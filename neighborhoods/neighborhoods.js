cc74ca94ae85a9ed343212cd97f55b607c741ea3
Lyzi Diamond <lyzidiamond@gmail.com>
2014-03-05 17:43:36 -0800

    style

--- neighborhoods/neighborhoods.js
+++ neighborhoods/neighborhoods.js
@@ -11,18 +11,18 @@ function onEachFeature(feature, layer) {
 
 function style(feature) {
   switch (feature.properties.DISTRICT) {
-    case 1: return {color: "#8dd3c7"};
-    case 2: return {color: "#ffffb3"};
-    case 3: return {color: "#bebada"};
-    case 4: return {color: "#fb8072"};
-    case 5: return {color: "#80b1d3"};
-    case 6: return {color: "#fdb462"};
-    case 7: return {color: "#b3de69"};
-    case 8: return {color: "#fccde5"};
-    case 9: return {color: "#d9d9d9"};
-    case 10: return {color: "#bc80bd"};
-    case 11: return {color: "#ccebc5"};
-    case 12: return {color: "#ffed6f"};
+    case 1: return {color: "#8dd3c7", weight: 0.2, fillOpacity: 0.8};
+    case 2: return {color: "#ffffb3", weight: 0.2, fillOpacity: 0.8};
+    case 3: return {color: "#bebada", weight: 0.2, fillOpacity: 0.8};
+    case 4: return {color: "#fb8072", weight: 0.2, fillOpacity: 0.8};
+    case 5: return {color: "#80b1d3", weight: 0.2, fillOpacity: 0.8};
+    case 6: return {color: "#fdb462", weight: 0.2, fillOpacity: 0.8};
+    case 7: return {color: "#b3de69", weight: 0.2, fillOpacity: 0.8};
+    case 8: return {color: "#fccde5", weight: 0.2, fillOpacity: 0.8};
+    case 9: return {color: "#d9d9d9", weight: 0.2, fillOpacity: 0.8};
+    case 10: return {color: "#bc80bd", weight: 0.2, fillOpacity: 0.8};
+    case 11: return {color: "#ccebc5", weight: 0.2, fillOpacity: 0.8};
+    case 12: return {color: "#ffed6f", weight: 0.2, fillOpacity: 0.8};
   };
 };
 
