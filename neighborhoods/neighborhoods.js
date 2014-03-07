5babf9eb81cd33279e8717331a2435a621425792
Lyzi Diamond <lyzidiamond@gmail.com>
2014-03-07 11:31:09 -0800

    change color scheme

--- neighborhoods/neighborhoods.js
+++ neighborhoods/neighborhoods.js
@@ -10,44 +10,44 @@ function onEachFeature(feature, layer) {
 };
 
 function style(feature) {
-  var shared = { stroke: "#111", fillOpacity: 0.6 };
+  var shared = { stroke: "#111", stroke-width: 0.1, fillOpacity: 0.6 };
   var ind;
   switch (feature.properties.DISTRICT) {
     case 1:
-      ind = { color: "#8dd3c7" };
+      ind = { color: "#a6cee3" };
       break;
     case 2:
-      ind = { color: "#ffffb3" };
+      ind = { color: "#1f78b4" };
       break;
     case 3:
-      ind = { color: "#bebada" };
+      ind = { color: "#b2df8a" };
       break;
     case 4:
-      ind = { color: "#fb8072" };
+      ind = { color: "#33a02c" };
       break;
     case 5:
-      ind = { color: "#80b1d3" };
+      ind = { color: "#fb9a99" };
       break;
     case 6:
-      ind = { color: "#fdb462" };
+      ind = { color: "#e31a1c" };
       break;
     case 7:
-      ind = { color: "#b3de69" };
+      ind = { color: "#fdbf6f" };
       break;
     case 8:
-      ind = { color: "#fccde5" };
+      ind = { color: "#ff7f00" };
       break;
     case 9:
-      ind = { color: "#d9d9d9" };
+      ind = { color: "#cab2d6" };
       break;
     case 10:
-      ind = { color: "#bc80bd" };
+      ind = { color: "#6a3d9a" };
       break;
     case 11:
-      ind = { color: "#ccebc5" };
+      ind = { color: "#ffff99" };
       break;
     case 12:
-      ind = { color: "#ffed6f" };
+      ind = { color: "#b15928" };
       break;
   };
   return $.extend(shared, ind);
