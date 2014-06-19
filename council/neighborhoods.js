c59861a2ccf1fc0996b5912439675d848eb1cf92
Lyzi Diamond <lyzidiamond@gmail.com>
2014-06-19 14:09:16 -0700

    Add council JS

---
+++ council/neighborhoods.js
@@ -0,0 +1,31 @@
+// initialize map
+var map = L.mapbox.map('neighborhoods-map', 'codeforamerica.hek4o94g', {maxZoom: 15, minZoom: 10}).setView([38.042,-84.515], 10);
+
+var colors = {
+  "1": "#c6dbef",
+  "2": "#6baed6",
+  "3": "#c6dbef",
+  "4": "#6baed6",
+  "5": "#c6dbef",
+  "6": "#6baed6",
+  "7": "#c6dbef",
+  "8": "#2171b5",
+  "9": "#c6dbef",
+  "10": "#2171b5",
+  "11": "#c6dbef",
+  "12": "#2171b5"
+};
+
+function style(feature) {
+  var shared = { stroke: "#111", strokeWidth: 0.1, fillOpacity: 0.6 };
+  var ind = colors[feature.properties.District];
+  return $.extend(shared, ind);
+};
+
+$.getJSON('./council.geojson', function(data) {
+  var councilDistricts = L.geoJson(data, {
+    style: style
+  });
+  councilDistricts.addTo(map);
+//  map.fitBounds(locations.getBounds(), {reset: true});
+});
